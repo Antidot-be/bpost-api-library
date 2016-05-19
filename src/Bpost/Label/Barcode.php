8a23833c9b0ea3c17bd2386e61306814c9017691
Gaël Lebrun <glebrun@antidot.com>
2016-05-19 16:41:13 +0200

    refactor (MultipleBarcodes): Create Barcode class

---
+++ src/Bpost/Label/Barcode.php
@@ -0,0 +1,37 @@
+<?php
+namespace Bpost\BpostApiClient\Bpost\Label;
+
+
+/**
+ * Class Barcode
+ * @package Bpost\BpostApiClient\Bpost\Label
+ */
+class Barcode {
+	/** suffix present on a barcode of return type */
+	const BARCODE_TYPE_RETURN_SUFFIX = '050';
+
+	/** @var string */
+	private $barcode;
+
+	/**
+	 * @param string $barcode
+	 */
+	public function __construct( $barcode ) {
+		$this->barcode = $barcode;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getBarcode() {
+		return $this->barcode;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isReturnBarcode() {
+		return substr( $this->barcode, -3) === self::BARCODE_TYPE_RETURN_SUFFIX;
+	}
+
+}
